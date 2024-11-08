dee5ddb3c0c976e54f986be1d7fd92d9628f8bbc
Quirianyailet Perez <qperez1@usf.edu>
2024-11-08 16:23:07 -0500

    Create transaction module with Transaction class.

---
+++ project-folder/transaction.js
@@ -0,0 +1,25 @@
+// Import named exports
+import {assets, getAssetById} from './assets.js';
+
+// Default export: Transaction class
+class Transaction {
+    constructor(assetId,type,quantity) {
+        this.assetId = assetId;
+        this.type = type;
+        this.quantity = quantity;
+    }
+    
+    updateQuantity(assetId) {
+    assetId = getAssetById;
+    if (type === buy) {
+        return  assets.quantity + this.quantity 
+    }
+    if (type === sell) {
+        return  assets.quantity - this.quantity
+    }
+    if (this.quantity > assets.quantity) {
+        throw new Error('Insufficient quantity for sale of [Asset Name]');
+}
+}
+}
+export default Transaction;
