19f92def1e276a11a0e22e2c8930660743d951ee
Quirianyailet Perez <qperez1@usf.edu>
2024-11-08 19:08:36 -0500

    update 5

--- project-folder/main.js
+++ project-folder/main.js
@@ -9,7 +9,7 @@ console.log(getPortfolioAllocation());
 const transaction1 = new Transaction (1,'buy',10);
 const transaction2 = new Transaction (2,'buy', 20);
 const transaction3 = new Transaction (3,'sell', 10);
-
+// Logging new transactions 
 console.log(transaction1);
 console.log(transaction2);
 console.log(transaction3);
