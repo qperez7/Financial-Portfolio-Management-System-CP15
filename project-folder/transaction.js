d37970d9c226edc3e95a07e99b750175ae80ade4
Quirianyailet Perez <qperez1@usf.edu>
2024-11-10 14:22:25 -0500

    Fixing Transactions class 2

--- project-folder/transaction.js
+++ project-folder/transaction.js
@@ -11,7 +11,7 @@ class Transaction {
         return getAssetById(assetId)
     }
     // Adds quantity & in this case adds Transaction
-    addQuantity(asset,type,quantity) {
+    addQuantity(assetId,type,quantity) {
     let asset = this.findAsset(assetId)
     if (type === 'buy') {
         return  quantity += asset.quantity 
