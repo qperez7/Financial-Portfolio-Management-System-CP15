2b1e196fa383a2d87f6a689a58e084a227a911b9
Quirianyailet Perez <qperez1@usf.edu>
2024-11-10 14:31:22 -0500

    Declaring new Transactions

--- project-folder/main.js
+++ project-folder/main.js
@@ -6,7 +6,10 @@ console.log (calculatePortfolioValue());
 console.log(getPortfolioAllocation());
 
 // Adding new transactions
-const newTransactions = new Transaction(assetId,type,quantity);
+let assetId = 0
+let type = 'stock'
+let quantity = 1 
+const newTransactions = new Transaction(assetId,type,quantity); // Gave me an error so I declared it to fix this
 newTransactions.addQuantity(1,'buy',10);
 newTransactions.addQuantity(2,'buy', 20);
 newTransactions.addQuantity(3,'sell', 10);
